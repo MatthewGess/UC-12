243dad3af56daa9809dc642d982692384669fb15
MatthewGess <torresvilla79@gmail.com>
2017-03-07 08:05:03 -0800

    deleted a duplicate function

--- functions.js
+++ functions.js
@@ -22,28 +22,6 @@ function between(string, start, end) {
 
     return string.slice(startAt, endAt);
 }
-/**
- * Returns an area code from a phone number: (###) ###-####
- * @param   {string} phoneNum The phone number
- * @returns {string} The area code
- * @throws {Error} If the format is incorrect
- */
-function getAreaCode(phoneNum) {
-
-    var areaCode;
-
-    try {
-        areaCode = between(phoneNum, "(", ")");
-        areaCode = areaCode.trim();
-        if (areaCode.length == 3 && Number(areaCode)) {
-            return areaCode;
-        } else {
-            throw new Error("Invalid area code: " + areaCode);
-        }
-    } catch (error) {
-        throw new Error("Invalid phone number: " + error.message);
-    }
-}
 
 /**
  * Displays the area code for an inputted phone number
@@ -64,8 +42,4 @@ function displayAreaCode(inputId, outputId) {
     }
 
     document.getElementById(outputId).innerHTML = outputText;
-}
-function getCoCode(phoneNum) {
-    var CoCode;
-
 }
